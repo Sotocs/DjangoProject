6055c519aaa4a0aaf227252797f13b63b35832c7
Sotocs <Nickolay.Klochkov@mail.ru>
2026-07-05 21:30:38 +0300

    HW 1 done

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,11 @@
+
 from django.contrib import admin
 from django.urls import path, include
+
+import catalog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog')),
+    path('contacts/', catalog.views.contacts, name='contacts'),
 ]

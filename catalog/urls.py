c93c4ec44eba296e8f27c8a26d92bd31b21b442a
Sotocs <Nickolay.Klochkov@mail.ru>
2026-07-05 21:35:23 +0300

    black done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import include, path
 
 from catalog import views
 from catalog.apps import CatalogConfig
@@ -7,6 +7,6 @@ from catalog.views import home
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('/contacts/', views.contacts, name='contacts')
+    path("", views.home, name="home"),
+    path("/contacts/", views.contacts, name="contacts"),
 ]

fac285218b636ce2a82cb7079edad8cd58baaebd
Sotocs <Nickolay.Klochkov@mail.ru>
2026-07-19 23:28:43 +0300

    product_detail.html done

--- catalog/urls.py
+++ catalog/urls.py
@@ -9,4 +9,5 @@ app_name = CatalogConfig.name
 urlpatterns = [
     path("", views.home, name="home"),
     path("contacts/", views.contacts, name="contacts"),
+    path('product/<int:pr_id>/', views.product_detail, name='product_detail'),
 ]
